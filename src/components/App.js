73b53b12e05e370e22991a10911b1215b9ff5fb1
CollinsTshuma <62089415+CollinsTshuma@users.noreply.github.com>
2022-03-14 06:57:36 +0200

    feat: Create Etherium Starter

--- src/components/App.js
+++ src/components/App.js
@@ -15,13 +15,8 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
+                <img src={logo} className="App-logo" alt="logo" />
+
                 <h1>Etherium Starter Kit</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
